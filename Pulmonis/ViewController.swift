8dc6e533bdfe610fe891fff213d05dd4b66f957a
Solan Manivannan <solan1803@gmail.com>
2016-10-17 12:20:29 +0100

    First page updated with new graphics

--- Pulmonis/ViewController.swift
+++ Pulmonis/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
         // Do any additional setup after loading the view, typically from a nib.
         
     }
