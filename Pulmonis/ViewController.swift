847736635b597ef00f656481371adebfc95bb227
Manivannan Solan <manivannansolan@dyn1182-134.wlan.ic.ac.uk>
2016-10-18 17:04:36 +0100

    main page almost finished, need to add notification view to it. Also added Liberation Sans font to project. Added inhaler with circle image to project.

--- Pulmonis/ViewController.swift
+++ Pulmonis/ViewController.swift
@@ -8,14 +8,25 @@
 
 import UIKit
 
+extension UILabel{
+    func addTextSpacing(spacing: CGFloat){
+        let attributedString = NSMutableAttributedString(string: self.text!)
+        attributedString.addAttribute(NSKernAttributeName, value: spacing, range: NSRange(location: 0, length: self.text!.characters.count))
+        self.attributedText = attributedString
+    }
+}
+
 class ViewController: UIViewController {
     @IBOutlet weak var learnImage: UIImageView!
+    @IBOutlet weak var pulmonisTitle: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.isTranslucent = true
+        pulmonisTitle.text = "PULMONIS"
+        pulmonisTitle.addTextSpacing(spacing: 3)
         // Do any additional setup after loading the view, typically from a nib.
         
     }
