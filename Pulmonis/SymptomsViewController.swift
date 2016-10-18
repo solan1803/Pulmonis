476f1e1321084fe2bb305612d2ad366ec540869d
Karow <karow.maruf14@imperial.ac.uk>
2016-10-18 14:34:18 +0100

    Completed Medicine page

--- Pulmonis/SymptomsViewController.swift
+++ Pulmonis/SymptomsViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class SymptomsViewController: UIViewController {
 
-    @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var symptomsButton: UIButton!
+    @IBOutlet weak var breathlessButton: UIButton!
+    @IBOutlet weak var inhalerButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        myButton.titleLabel?.textAlignment = NSTextAlignment.center
+        
+        symptomsButton.layer.cornerRadius = 10
+        symptomsButton.clipsToBounds = true
+        
+        breathlessButton.layer.cornerRadius = 10
+        breathlessButton.clipsToBounds = true
+        
+        inhalerButton.layer.cornerRadius = 10
+        inhalerButton.clipsToBounds = true
+        
+        nextButton.layer.cornerRadius = 10
+        nextButton.clipsToBounds = true
+        
+        inhalerButton.titleLabel?.textAlignment = NSTextAlignment.center
 //        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
 //        self.navigationController?.navigationBar.shadowImage = UIImage()
 //        self.navigationController?.navigationBar.isTranslucent = true
