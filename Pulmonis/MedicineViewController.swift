a4105a004eefbc66e9dc8afb5133b0a844a7b399
Karow <karow.maruf14@imperial.ac.uk>
2016-10-18 13:39:58 +0100

    Starting working on Medicine page.

---
+++ Pulmonis/MedicineViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MedicineViewController.swift
+//  Pulmonis
+//
+//  Created by Karow Maruf on 17/10/2016.
+//  Copyright © 2016 Manivannan Solan. All rights reserved.
+//
+
+import UIKit
+
+class MedicineViewController: UIViewController {
+
+    @IBOutlet weak var numOfTabletsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        numOfTabletsLabel.backgroundColor = UIColor(patternImage: UIImage(named: "learn")!)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
