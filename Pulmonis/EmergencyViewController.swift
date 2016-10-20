0c974a1cd5195eadec1ba9d3b230e0c1ca6159a8
Seng, Sophie <sjs111@cider06.doc.ic.ac.uk>
2016-10-20 13:30:03 +0100

    Put views inside stackViews to aid layout design

--- Pulmonis/EmergencyViewController.swift
+++ Pulmonis/EmergencyViewController.swift
@@ -10,13 +10,36 @@ import UIKit
 
 class EmergencyViewController: UIViewController {
 
-    @IBOutlet weak var inhalerColourLabel: UILabel!
+    
+
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var bottomSentenceLabel: UILabel!
+    @IBOutlet weak var instructionLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        inhalerColourLabel.layer.cornerRadius = 10.0
-        inhalerColourLabel.clipsToBounds = true
+        nextButton.layer.cornerRadius = 10.0
+        nextButton.clipsToBounds = true
 
         // Do any additional setup after loading the view.
+        
+        let blueColour = UIColor (red:(118.0/255.0), green:(213.0/255.0), blue:(255.0/255.0), alpha:1)
+        
+        let headerAttributedString = NSMutableAttributedString(string: "Use your blue inhaler")
+        headerAttributedString.addAttribute(NSFontAttributeName, value: UIFont.boldSystemFont(ofSize: headerLabel.font.pointSize), range: NSRange(location:9,length:4))
+        headerAttributedString.addAttribute(NSBackgroundColorAttributeName, value: blueColour, range: NSRange(location:9,length:4))
+        headerAttributedString.addAttribute(NSForegroundColorAttributeName, value: UIColor.white, range: NSRange(location:9,length:4))
+        headerLabel.attributedText = headerAttributedString
+        
+        let bottomAttributedString = NSMutableAttributedString(string: "until you feel better")
+        bottomAttributedString.addAttribute(NSFontAttributeName, value: UIFont.boldSystemFont(ofSize: bottomSentenceLabel.font.pointSize), range: NSRange(location:10,length:11))
+        bottomSentenceLabel.attributedText = bottomAttributedString
+        
+        let instructionAttributedString = NSMutableAttributedString(string: "Push inhaler and breathe in")
+        instructionAttributedString.addAttribute(NSFontAttributeName, value: UIFont.boldSystemFont(ofSize: instructionLabel.font.pointSize), range: NSRange(location:16,length:11))
+        instructionLabel.attributedText = instructionAttributedString
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
