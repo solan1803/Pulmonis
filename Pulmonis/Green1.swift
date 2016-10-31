12baf3064f1955f61faa08a015493ec419b66ffb
Karow <karow.maruf14@imperial.ac.uk>
2016-10-31 14:43:35 +0000

    Created view controller for first Green section page.

---
+++ Pulmonis/Green1.swift
@@ -0,0 +1,38 @@
+//
+//  Green1.swift
+//  Pulmonis
+//
+//  Created by Karow Maruf on 31/10/2016.
+//  Copyright © 2016 Manivannan Solan. All rights reserved.
+//
+
+import UIKit
+
+class Green1: UIViewController {
+    @IBOutlet weak var gPreventerInhalerColour: UITextField!
+    @IBOutlet weak var gPuffsMorning: UITextField!
+    @IBOutlet weak var gPuffsNight: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
