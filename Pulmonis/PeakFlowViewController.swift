e39d6bd19e3c83b12c81d10bb859b425890d0e7d
Solan Manivannan <solan1803@gmail.com>
2016-11-01 15:31:11 +0000

    Created model for peak flow table, also managed to save data to managedObjectContext, fixed keyboard issue for all view controllers so that the keyboard will push the whole view up when the keyboard shows, and also when you touch outside, it dismisses the keyboard. Other pairs need to add three lines to their view controllers for this functionality.

---
+++ Pulmonis/PeakFlowViewController.swift
@@ -0,0 +1,104 @@
+//
+//  PeakFlowViewController.swift
+//  Pulmonis
+//
+//  Created by Manivannan Solan on 01/11/2016.
+//  Copyright © 2016 Manivannan Solan. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class PeakFlowViewController: UIViewController {
+
+    // MARK: Model
+    
+    // if this is nil, then we simply don't update the database
+    // having this default to the AppDelegate's context is a little bit of "demo cheat"
+    // probably it would be better to subclass TweetTableViewController
+    // and set this var in that subclass and then use that subclass in our storyboard
+    // (the only purpose of that subclass would be to pick what database we're using)
+    var managedObjectContext: NSManagedObjectContext? =
+        (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        // Do any additional setup after loading the view.
+    }
+    
+   
+
+    @IBAction func savePeakFlow(_ sender: UIButton) {
+        print("INSIDE SAVEPEAKFLOW METHOD")
+        if let peak_record = NSEntityDescription.insertNewObject(forEntityName: "PeakFlowTable", into: managedObjectContext!) as? PeakFlowTable {
+            peak_record.date = NSDate()
+            peak_record.value = 3
+        }
+        do {
+            try managedObjectContext?.save()
+        } catch let error {
+            print(error)
+        }
+        let request: NSFetchRequest<PeakFlowTable> = PeakFlowTable.fetchRequest()
+        let result = try? managedObjectContext!.fetch(request)
+        
+        for r in result! {
+            print("DATE: ")
+            print(r.date!)
+            print("VALUE: ")
+            print(r.value)
+        }
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+}
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0{
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0{
+                self.view.frame.origin.y += keyboardSize.height
+            }
+        }
+    }
+}
