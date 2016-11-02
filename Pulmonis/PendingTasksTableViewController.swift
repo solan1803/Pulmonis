500f9121a2493a5a9abb507957e7b546c9df47ab
Solan Manivannan <solan1803@gmail.com>
2016-11-02 17:38:41 +0000

    Started work on deleting pending tasks. Added settings icon to top right of main app page and this now links to the settings page (settings was previously linked to the map button).

--- Pulmonis/PendingTasksTableViewController.swift
+++ Pulmonis/PendingTasksTableViewController.swift
@@ -95,12 +95,14 @@ class PendingTasksTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            managedObjectContext?.delete(tasksList[indexPath.row])
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
     */
+    
 
     /*
     // Override to support rearranging the table view.
