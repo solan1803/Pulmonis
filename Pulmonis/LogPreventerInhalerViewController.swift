31d718b457a92e0a41617ffe73004ad6e2cc5788
Solan Manivannan <solan1803@gmail.com>
2016-11-14 23:48:05 +0000

    Redid main page based on feedback from uddhav. Now has two buttons to log reliever / preventer inhaler results, these bring up popups that allow the user to enter the data. Removed links to map/stats/learn. Deleted learn UI test because no longer linked. There will be three warnings since we have archived the previous main.storyboard just in case.

---
+++ Pulmonis/LogPreventerInhalerViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LogPreventerInhalerViewController.swift
+//  Pulmonis
+//
+//  Created by Manivannan Solan on 14/11/2016.
+//  Copyright © 2016 Manivannan Solan. All rights reserved.
+//
+
+import UIKit
+
+class LogPreventerInhalerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
