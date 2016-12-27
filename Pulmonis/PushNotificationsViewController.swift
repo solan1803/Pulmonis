f9bdcb3ab0d0414818d32b2d9e00cf96c2814cb5
Solan Manivannan <solan1803@gmail.com>
2016-12-27 12:55:06 +0000

    Push Notifications added to Pulmonis, implementation details are there and working including requesting authorisation from user to allow sending notifications.

---
+++ Pulmonis/PushNotificationsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PushNotificationsViewController.swift
+//  Pulmonis
+//
+//  Created by Manivannan Solan on 27/12/2016.
+//  Copyright © 2016 Manivannan Solan. All rights reserved.
+//
+
+import UIKit
+
+class PushNotificationsViewController: UIViewController {
+    
+    @IBAction func addReminderNotification(_ sender: UIButton) {
+        
+        let selectedDate = Date().addingTimeInterval(120)
+        let delegate = UIApplication.shared.delegate as? AppDelegate
+        delegate?.scheduleNotification(at: selectedDate)
+        
+    }
+}
