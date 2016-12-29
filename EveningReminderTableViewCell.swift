5d771da6e95efc52a2738b2c1540534db9572475
Solan Manivannan <solan1803@gmail.com>
2016-12-29 20:05:50 +0000

    Finished push notifications with plist.

--- EveningReminderTableViewCell.swift
+++ EveningReminderTableViewCell.swift
@@ -18,51 +18,82 @@ class EveningReminderTableViewCell: UITableViewCell {
     var pListVal = false
     
     @IBAction func eveningReminderSwitchChanged(_ sender: UISwitch) {
-        if(pListVal){
-            //want to turn off
-            let eveningNotification = ["eveningNotification"]
-            UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: eveningNotification)
-            pListVal = false
-        } else {
+        
+        if let plist = Plist(name: "Reminders") {
             
-            var puffs = ""
-            
-            if let plist = Plist(name: "PatientData") {
-                let dict = plist.getMutablePlistFile()!
+            let dict = plist.getMutablePlistFile()!
+            let eveningInfo = dict["evening"]! as? [Any]
+            if(eveningInfo?.count != 0 && (eveningInfo?[1] as? Bool)!){
+                print("[DEBUG] Attempting to set evening reminder to off in plist")
+                let eveningNotification = ["eveningNotification"]
+                UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: eveningNotification)
                 
-                if let eveningPuffs = (dict[gPuffsNightStr]! as? String) {
-                    puffs = eveningPuffs
+                dict["evening"] = ["6pm date", false]
+                do {
+                    try plist.addValuesToPlistFile(dictionary: dict)
+                } catch {
+                    print(error)
                 }
             } else {
-                //Error with opening the PList
-            }
-            
-            
-            let calendar = Calendar(identifier: .gregorian)
-            let newComponents = DateComponents(calendar: calendar, timeZone: .current, second: 0)
-            
-            let trigger = UNCalendarNotificationTrigger(dateMatching: newComponents, repeats: true)
-            
-            let content = UNMutableNotificationContent()
-            content.title = "Evening Asthma Reminder"
-            content.body = "Please remember to take \(puffs) puffs of your preventer inhaler this evening"
-            content.sound = UNNotificationSound.default()
-            
-            let request = UNNotificationRequest(identifier: "eveningNotification", content: content, trigger: trigger)
-            
-            UNUserNotificationCenter.current().add(request) {(error) in
-                if let error = error {
-                    print("Uh oh! We had an error: \(error)")
+                print("[DEBUG] Attempting to set evening reminder to on in plist.")
+                var puffs = ""
+                
+                if let plist = Plist(name: "PatientData") {
+                    let dict = plist.getMutablePlistFile()!
+                    
+                    if let eveningPuffs = (dict[gPuffsNightStr]! as? String) {
+                        puffs = eveningPuffs
+                    }
+                } else {
+                    //Error with opening the PList
+                }
+                
+                
+                let calendar = Calendar(identifier: .gregorian)
+                let newComponents = DateComponents(calendar: calendar, timeZone: .current, second: 0)
+                
+                let trigger = UNCalendarNotificationTrigger(dateMatching: newComponents, repeats: true)
+                
+                let content = UNMutableNotificationContent()
+                content.title = "Evening Asthma Reminder"
+                content.body = "Please remember to take \(puffs) puffs of your preventer inhaler this evening"
+                content.sound = UNNotificationSound.default()
+                
+                let request = UNNotificationRequest(identifier: "eveningNotification", content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request) {(error) in
+                    if let error = error {
+                        print("Uh oh! We had an error: \(error)")
+                    }
+                }
+                
+                dict["evening"] = ["6pm date", true]
+                do {
+                    try plist.addValuesToPlistFile(dictionary: dict)
+                } catch {
+                    print(error)
                 }
             }
-            pListVal = true
-            
         }
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        if let plist = Plist(name: "Reminders") {
+            let dict = plist.getMutablePlistFile()!
+            
+            if let eveningReminderInfo = (dict["evening"]! as? [Any]) {
+                if eveningReminderInfo.count != 0 {
+                    if let switchVal = eveningReminderInfo[1] as? Bool {
+                        eveningSwitch.setOn(switchVal, animated: false)
+                    }
+                }
+            }
+        } else {
+            //Error with opening the PList
+        }
+        
         if let plist = Plist(name: "PatientData") {
             let dict = plist.getMutablePlistFile()!
             
