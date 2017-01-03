009fb322ca7f296356bdf39d6f1c26cb3d7d6931
Sophie Seng <sjs111@ic.ac.uk>
2017-01-03 14:39:53 +0000

    Further enhancements to weather data plotting, view theme now matches app theme, handles erroneous coordinates

--- Pulmonis/PollenGETRequests.swift
+++ Pulmonis/PollenGETRequests.swift
@@ -13,42 +13,6 @@ let baseURL : String = "http://dataservice.accuweather.com"
 let locationURL : String = "/locations/v1/cities/geoposition/search"
 let forecastURL : String = "/forecasts/v1/daily/5day/"
 
-public func getLocationKey(latitude : Double, longitude : Double) {
-    let request = NSMutableURLRequest(url: generateLocationURL(latitude: latitude, longitude: longitude) as URL)
-    let session = URLSession.shared
-    
-    request.httpMethod = "GET"
-    let _: NSError?
-
-    
-    let task = session.dataTask(with: request as URLRequest, completionHandler: {data, response, err -> Void in
-        
-        if err != nil {
-            print(err!)
-        } else {
-            
-            do {
-                let parsedData = try JSONSerialization.jsonObject(with: data!, options: [.allowFragments]) as! [String:Any]
-            
-                let locationKey = parsedData["Key"] as! String
-                let localisedName = parsedData["LocalizedName"] as! String
-                
-                let administrativeArea = parsedData["AdministrativeArea"] as! [String:Any]
-                
-                let localisedAdministrativeName = administrativeArea["LocalizedName"] as! String
-                
-                
-                print(locationKey, localisedName, localisedAdministrativeName)
-                //getWeatherForecast(locationKey: locationKey, name: localisedName, area: localisedAdministrativeName)
-            } catch {
-                print("error serializing JSON: \(error)")
-            }
-        }
-    })
-    
-    task.resume()
-    
-}
 
 public func generateLocationURL(latitude : Double, longitude : Double) -> NSURL {
     var url = baseURL + locationURL
@@ -64,45 +28,3 @@ public func generateForecastURL(key : String) -> NSURL {
     return NSURL(string: url)!
 }
 
-public func getWeatherForecast(locationKey : String, name : String, area : String) {
-    let request = NSMutableURLRequest(url: generateForecastURL(key: locationKey) as URL)
-    let session = URLSession.shared
-    
-    request.httpMethod = "GET"
-    let _: NSError?
-    
-    
-    let task = session.dataTask(with: request as URLRequest, completionHandler: {data, response, err -> Void in
-        
-        var parsedForecasts = [String]()
-        
-        if err != nil {
-            print(err!)
-        } else {
-            
-            do {
-                let parsedData = try JSONSerialization.jsonObject(with: data!, options: []) as! [String:Any]
-                
-                if  let forecasts = parsedData["DailyForecasts"] as? [[String: Any]] {
-                    for forecast in forecasts {
-                        if let name = forecast["name"] as? String {
-                            //names.append(name)
-                        }
-                        if let name = forecast["name"] as? String {
-                            //names.append(name)
-                        }
-                        if let name = forecast["name"] as? String {
-                            //names.append(name)
-                        }
-                    }
-                }
-                
-            } catch {
-                print("error serializing JSON: \(error)")
-            }
-        }
-    })
-    
-    task.resume()
-    
-}
